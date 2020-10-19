93ad5d0774b54f545eb681117a4b6998dd5e8a40
Alex Wander <alexcwander@gmail.com>
2020-10-19 13:06:47 -0500

    Build routes and tested using Postman

--- server.js
+++ server.js
@@ -6,4 +6,9 @@ app.get('/', (req, res) =>
 	res.json({ msg: 'Welcome to the ContactKeeper API' })
 );
 
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
