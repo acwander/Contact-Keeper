82571ca75fcc372b203332d063b72e3e447a4e6e
Alex Wander <alexcwander@gmail.com>
2020-10-19 12:20:40 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) =>
+	res.json({ msg: 'Welcome to the ContactKeeper API' })
+);
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
