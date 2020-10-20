41238c997bda8f376769770bb034a2beea8d67e6
Alex Wander <alexcwander@gmail.com>
2020-10-20 17:29:25 -0500

    Create User model

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 5000;
 // Connect DB
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) =>
 	res.json({ msg: 'Welcome to the ContactKeeper API' })
 );
