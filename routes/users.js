d932dbf4adcc59467c7e81f89e4dab78c936df68
Alex Wander <alexcwander@gmail.com>
2020-10-20 18:50:42 -0500

    Add user to db with hashed password

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
+const { genSalt } = require('bcryptjs');
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { body, validationResult } = require('express-validator');
 
 const User = require('../models/User');
@@ -20,15 +22,43 @@ router.post(
 			'Enter a valid password with 6 or more characters'
 		).isLength({ min: 6 }),
 	],
-	(req, res) => {
+	async (req, res) => {
 		// Finds the validation errors in this request and wraps them in an object with handy functions
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			// Send array of errors
 			return res.status(400).json({ errors: errors.array() });
 		}
-		// Send success if all checks are met
-		res.send('success');
+
+		// De-structure data
+		const { name, email, password } = req.body;
+
+		try {
+			let user = await User.findOne({ email });
+
+			// If user already exists, throw an error
+			if (user) {
+				return res.status(400).json({ msg: 'User already exists' });
+			}
+
+			// Create user
+			user = new User({
+				name,
+				email,
+				password,
+			});
+
+			const salt = await bcrypt.genSalt(10);
+			// Hash the password
+			user.password = await bcrypt.hash(password, salt);
+			// Add user to db
+			await user.save();
+
+			res.send('User saved');
+		} catch (error) {
+			console.error(error.message);
+			res.status(500).send('Server Error');
+		}
 	}
 );
 
